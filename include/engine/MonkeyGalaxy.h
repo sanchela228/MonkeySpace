3a3ba8d6903ec236caf30fdb6ccea626adf40388
mikro <nikwoochach@yandex.ru>
2025-01-30 22:07:08 +0500

    add files models

--- include/engine/MonkeyGalaxy.h
+++ include/engine/MonkeyGalaxy.h
@@ -1,5 +1,7 @@
 #ifndef MONKEYGALAXY_H
 #define MONKEYGALAXY_H
+#include <string>
+
 #include "types/Vector3.h"
 
 using namespace Types;
@@ -7,15 +9,23 @@ using namespace Types;
 namespace Engine {
     struct MonkeyGalaxySettings {
         Vector3 size;
-        int min_count_systems;
-        int max_count_systems;
+        std::string seed;
+
+        int min_count_systems = 1;
+        int max_count_systems = 5;
+        int min_count_entities_per_system = 1;
+        int max_count_entities_per_system = 5;
+        int min_count_children_per_entity = 0;
+        int max_count_children_per_entity = 5;
+
+        bool systems_without_stars = false;
+        char percent_star_in_system = 80;
     };
 
     struct MonkeyGalaxy {
-        MonkeyGalaxy generation(MonkeyGalaxySettings settings);
+        std::vector<Entity*> entities;
+        MonkeyGalaxy generate(MonkeyGalaxySettings settings);
     };
-
-
 
 }
 
